38d11f969f23e77d987039eecc20312ea6205d26
Diego Reyes <diegorele13@gmail.com>
2025-09-23 10:24:14 -0500

    feat(routes): add auth routes to api router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import express, { Application } from "express";
+import authRouter from "./auth.route";
 
 function routerApi(app: Application) {
   const router = express.Router();
   app.use("/api", router);
-
+  
+  // Auth routes
+  router.use("/auth", authRouter);
 }
 
 export default routerApi;
